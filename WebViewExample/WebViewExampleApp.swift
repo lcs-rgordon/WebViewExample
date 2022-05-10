332a2d80b47f4afe02b5488bd226e7a27929e192
Russell Gordon <github@russellgordon.ca>
2022-05-10 05:46:30 -0400

    Initial Commit

---
+++ WebViewExample/WebViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewExampleApp.swift
+//  WebViewExample
+//
+//  Created by Russell Gordon on 2022-05-10.
+//
+
+import SwiftUI
+
+@main
+struct WebViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
